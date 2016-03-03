13d20bf47829bc5d1ebc1663db0c2550bd90b0c9
zyf14 <zyf14@software.nju.edu.cn>
2016-03-03 19:03:26 +0800

    项目新建

---
+++ GitMining/src/main/java/LifeIS/GitMining/App.java
@@ -0,0 +1,13 @@
+package LifeIS.GitMining;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
