bd4aafb4999d2be3a5b7fa85076cd15ef2dc89d6
zyf14 <zyf14@software.nju.edu.cn>
2016-03-03 21:08:11 +0800

    Development: 无用类删除

--- GitMining/src/main/java/data/dataServer/ProjectDataServer.java
+++ GitMining/src/main/java/data/dataServer/ProjectDataServer.java
@@ -5,11 +5,12 @@ public interface ProjectDataServer {
 	/**
 	 * @return 返回所有项目信息 
 	 */
-	public String getAllProjects();
+	public String getAllProjects()throws Exception;
 	
 	/**
 	 * 
 	 * @return 返回单个项目的信息 
 	 */
 	public String getProjectByName(String name);
+	
 }
