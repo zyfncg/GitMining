81e3dd45866d1770dc20ec2ddd86d77a8ac026fd
rww14 <rww14@software.nju.edu.cn>
2016-03-10 20:26:10 +0800

    Debug:Search算法修正

--- GitMining/src/main/java/businessLogic/businessLogicModel/RepositoryModel/AllSort/ForkSort.java
+++ GitMining/src/main/java/businessLogic/businessLogicModel/RepositoryModel/AllSort/ForkSort.java
@@ -11,8 +11,8 @@ public class ForkSort implements DetailSort{
 
 	@Override
 	public List<ProjectInfo> MakeSort(List<ProjectInfo> projectInfos) {
-		// TODO Auto-generated method stub
-		 Collections.sort(projectInfos,new Comparator<ProjectInfo>(){
+
+		Collections.sort(projectInfos,new Comparator<ProjectInfo>(){
 	            @Override
 				public int compare(ProjectInfo arg0, ProjectInfo arg1) {
 	                return (new Integer(arg1.getForks())).compareTo(new Integer(arg0.getForks()));
