101b8d99fd56cd2cf7ed80a3646c4c78ed6a5f92
zyf14 <zyf14@software.nju.edu.cn>
2016-03-21 11:46:04 +0800

    Document:第四周个人总结

--- GitMining/src/main/java/data/dataImpl/StatisticData.java
+++
@@ -1,5 +0,0 @@
-package data.dataImpl;
-
-public class StatisticData {
-
-}
---
+++ GitMining/src/main/java/data/statistisDataImpl/UserStatisticData.java
@@ -0,0 +1,31 @@
+package data.statistisDataImpl;
+
+import java.util.List;
+
+import Info.UserInfoDetail;
+import Info.UsrStatisticInfo.SaveUserStatisticInfo;
+import data.statisticServer.UserStatisticsDataServer;
+
+public class UserStatisticData implements UserStatisticsDataServer{
+
+	@Override
+	public List<UserInfoDetail> getStatisticUsersInfo() throws Exception {
+		// TODO Auto-generated method stub
+		
+		
+		return null;
+	}
+
+	@Override
+	public boolean setStatisticInfo(SaveUserStatisticInfo saveUser) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public SaveUserStatisticInfo GetStatisticedInfo() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
