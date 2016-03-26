e272d776b2c811b146d4563cc2766bb61edd7a6d
zrz14 <zrz14@software.nju.edu.cn>
2016-03-26 09:59:27 +0800

    Development:  图表初步完成，暂缺数据调用

---
+++ GitMining/src/main/java/ui/chart/NumbersofRepositoryInvolved.java
@@ -0,0 +1,45 @@
+package ui.chart;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+import twaver.Element;
+import twaver.Node;
+import twaver.TDataBox;
+import twaver.TWaverConst;
+import twaver.chart.LineChart;
+
+public class NumbersofRepositoryInvolved extends JPanel{
+
+	public NumbersofRepositoryInvolved (int width, int height) {
+		TDataBox box = new TDataBox();		
+		LineChart lineChart = new LineChart(box);
+		
+		lineChart.setYAxisVisible(true);
+		lineChart.setYScaleTextVisible(true);
+		lineChart.setXAxisVisible(true);
+		lineChart.setXScaleTextVisible(true);
+		//每个点上是否需要显示标记
+		lineChart.setInflexionVisible(true);
+		
+		Element A = new Node();
+		A.setName("Repository");
+		A.putChartColor(Color.GREEN);
+		//设置标记的显示样式
+		A.putChartInflexionStyle(TWaverConst.INFLEXION_STYLE_TRIANGLE);
+		box.addElement(A);
+		//TODO 获取参与项目数量
+		for (int i = 2007; i < 2016; i++) {
+			lineChart.addXScaleText(" " + i);
+			A.addChartValue(Math.random() * 50);
+		}
+		
+		lineChart.setPreferredSize(new Dimension(width, height));
+		this.setOpaque(false);
+		this.setLayout(new BorderLayout());
+		this.add(lineChart, BorderLayout.CENTER);
+	}
+}
