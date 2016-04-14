6221875178b346ae22185264ee64d101b5d69a12
rww14 <rww14@software.nju.edu.cn>
2016-04-14 15:24:37 +0800

    develop:同步

--- GitMining/src/main/java/start/Setup.java
+++ GitMining/src/main/java/start/Setup.java
@@ -1,5 +1,8 @@
 package start;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import ui.MainFrame;
 
 /**
@@ -9,5 +12,21 @@ public class Setup {
 
 	public static void main(String[] args) {
 		new MainFrame();
+		String lookAndFeel =  "javax.swing.plaf.metal.MetalLookAndFeel";
+		try {
+			UIManager.setLookAndFeel(lookAndFeel);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
