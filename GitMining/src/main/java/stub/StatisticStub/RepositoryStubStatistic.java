8b11da50f57d5f1d343b00b46c96025c3615fd38
rww14 <rww14@software.nju.edu.cn>
2016-04-14 15:56:23 +0800

    develop:完善

--- GitMining/src/main/java/stub/StatisticStub/RepositoryStubStatistic.java
+++ GitMining/src/main/java/stub/StatisticStub/RepositoryStubStatistic.java
@@ -3,18 +3,46 @@ package stub.StatisticStub;
 import java.util.ArrayList;
 import java.util.List;
 
+import Info.RepStatisticInfo.ContributorStatistics;
 import Info.RepStatisticInfo.ForkStatistics;
 import Info.RepStatisticInfo.LanguageStatistics;
 import Info.RepStatisticInfo.SaveRepositoryStatisticInfo;
+import Info.RepStatisticInfo.StarStatistics;
+import Info.RepStatisticInfo.TimeStatistics;
 
 public class RepositoryStubStatistic {
 
-	public SaveRepositoryStatisticInfo GetStatisticedInfo() throws Exception{
-		
+	public SaveRepositoryStatisticInfo GetStatisticedInfo() throws Exception {
+
 		List<ForkStatistics> forkResult = new ArrayList<ForkStatistics>();
 		List<LanguageStatistics> languageResult = new ArrayList<LanguageStatistics>();
-		
-		
-		return null;
+		List<ContributorStatistics> contributorResult = new ArrayList<ContributorStatistics>();
+		List<StarStatistics> starResult = new ArrayList<StarStatistics>();
+		List<TimeStatistics> timeResult = new ArrayList<TimeStatistics>();
+
+		for (int i = 0; i < 7; i++) {
+			ForkStatistics forkStatistics = new ForkStatistics("GG", 10);
+			forkResult.add(forkStatistics);
+		}
+		for (int i = 0; i < 7; i++) {
+			LanguageStatistics languageStatistics = new LanguageStatistics("java", i, 12.0);
+			languageResult.add(languageStatistics);
+		}
+		for (int i = 0; i < 7; i++) {
+			ContributorStatistics contributorStatistics = new ContributorStatistics();
+			contributorResult.add(contributorStatistics);
+		}
+		for (int i = 0; i < 7; i++) {
+			StarStatistics starStatistics = new StarStatistics("GL", i);
+			starResult.add(starStatistics);
+		}
+		for (int i = 0; i < 7; i++) {
+			TimeStatistics timeStatistics = new TimeStatistics("1995", i);
+			timeResult.add(timeStatistics);
+		}
+
+		SaveRepositoryStatisticInfo saveRepositoryStatisticInfo = new SaveRepositoryStatisticInfo(timeResult,
+				languageResult, forkResult, starResult, contributorResult);
+		return saveRepositoryStatisticInfo;
 	}
 }
--- GitMining/src/test/java/StatisticTest/HistogramTest.java
+++ GitMining/src/test/java/StatisticTest/HistogramTest.java
@@ -1,22 +1,29 @@
 package StatisticTest;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 import Info.HistogramInfo;
 import RepositoryStatistic.GetRepositoryStatistic.HistogramStatisticRep;
 import RepositoryStatistic.GetRepositoryStatistic.RepositoryStatisticFactory;
 
 public class HistogramTest {
+	@Test
 	public void test(){
 		RepositoryStatisticFactory factory = new RepositoryStatisticFactory();
 		HistogramStatisticRep histogramStatisticFork = factory.GetHistogramFork();
 		HistogramInfo histogramInfo = histogramStatisticFork.getRepHistogramInfo();
-		
-		System.out.println(histogramInfo.getGroupNum()+"  "+histogramInfo.getMaxNum()+"  "+histogramInfo.getMinNum());
+		assertEquals(histogramInfo.getGroupNum(),60);
+		assertEquals(histogramInfo.getMaxNum(), 9609);
+		assertEquals(histogramInfo.getMinNum(), 0);
+//		System.out.println(histogramInfo.getGroupNum()+"  "+histogramInfo.getMaxNum()+"  "+histogramInfo.getMinNum());
 		
 	}
 	
-	public static void main(String[] args) {
-		HistogramTest test = new HistogramTest();
-		test.test();
-	}
+//	public static void main(String[] args) {
+//		HistogramTest test = new HistogramTest();
+//		test.test();
+//	}
 	
 }
