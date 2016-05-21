8dfca2add1559733189d70492af5b630b306ee7d
zyf14 <zyf14@software.nju.edu.cn>
2016-05-21 11:11:53 +0800

    Development:地图接口实现

---
+++ GitMining/src/main/java/data/map/AddressImpl.java
@@ -0,0 +1,14 @@
+package data.map;
+
+import Info.AddressInfo;
+import data.statisticServer.AddressServer;
+
+public class AddressImpl implements AddressServer{
+
+	@Override
+	public AddressInfo getAddressByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
