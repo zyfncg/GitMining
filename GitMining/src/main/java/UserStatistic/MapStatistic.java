a873d7af0d8300003d32945febfedf2c87b17afc
zyf14 <zyf14@software.nju.edu.cn>
2016-05-21 16:43:57 +0800

    Development:地图数据处理测试

--- GitMining/src/main/java/UserStatistic/MapStatistic.java
+++ GitMining/src/main/java/UserStatistic/MapStatistic.java
@@ -6,14 +6,17 @@ import java.util.Map.Entry;
 
 import Info.AddressInfo;
 import Info.UserInfoDetail;
+import data.map.AddressImpl;
 import data.map.MapData;
+import data.statisticServer.AddressServer;
 import data.statisticServer.UserStatisticsDataServer;
 import data.statistisDataImpl.UserStatisticData;
 
 public class MapStatistic {
+	
 	public boolean StatisticMapData(){
-		MapData mapData=new MapData();
-		Map<String,AddressInfo> map=mapData.getMapData();
+		AddressServer addrData=new AddressImpl();
+		Map<String,AddressInfo> map=addrData.getMapData();
 		UserStatisticsDataServer userData=new UserStatisticData();
 		List<UserInfoDetail> userList = null;
 		try {
@@ -23,6 +26,7 @@ public class MapStatistic {
 			return false;
 		}
 		String address;
+		List<String> keyList;
 		AddressInfo addressInfo;
 		int workerNum;
 		
@@ -32,43 +36,26 @@ public class MapStatistic {
 		    addressInfo.setWorkerNumber(0);
 		    map.put(key, addressInfo);
 		}
-		int count = 0;
-		System.out.println(map.size());
+
+		
 		for(UserInfoDetail user :userList){
 			address=user.getAddress().toLowerCase();
-			if(address.contains("san francisco")&&!address.contains(", ca")){
-				address=", ca";
-			}else if(address.contains("seattle")&&!address.contains(", wa")){
-				address=", wa";
-			}else if(address.contains("united kingdom")||address.contains("england")){
-				address=", uk";
-			}else if(address.contains(", new york")){
-				address=", ny";
-			}else if(address.contains("brazil")){
-				address="brasil";
+			keyList=addrData.getMapKey(address);
+
+			if(keyList==null){
+				continue;
 			}
-//			System.out.println(address);
-			for(String key:map.keySet()){
-				
-				if(address.contains(key)){
-					count++;
-					System.out.println(count);
-					if(key.equals(", ca")&&address.contains("canada")){
-						continue;
-					}else if(key.equals(", in")&&address.contains("india")){
-						continue;
-					}
-					addressInfo=map.get(key);
-					workerNum=addressInfo.getWorkerNumber();
-					workerNum++;
-					addressInfo.setWorkerNumber(workerNum);
-					map.put(key, addressInfo);
-					
-				}
+			for(String key:keyList){
+				addressInfo=map.get(key);
+				workerNum=addressInfo.getWorkerNumber();
+				workerNum++;
+				addressInfo.setWorkerNumber(workerNum);
+				map.put(key, addressInfo);
 			}
+			
 		}
 		
-		if(mapData.setMapData(map)){
+		if(addrData.saveMapData(map)){
 			return true;
 		}else{
 			return false;
