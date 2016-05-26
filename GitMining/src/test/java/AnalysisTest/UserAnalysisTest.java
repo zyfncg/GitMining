c608f80c27fad0794f56be0c76d0595d14034e1b
rww14 <rww14@software.nju.edu.cn>
2016-05-26 13:27:54 +0800

    develop:同步

---
+++ GitMining/src/test/java/AnalysisTest/UserAnalysisTest.java
@@ -0,0 +1,34 @@
+package AnalysisTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Info.RelationUser;
+import Info.Relationship;
+import Info.UserInfo;
+import UserStatistic.GetUserStatistic.UserStatisticFactory;
+import UserStatistic.GetUserStatistic.UserAnalysis.RelationAnalysisStatic;
+import businessLogic.businessLogicModel.UserModel.UserHandle;
+
+public class UserAnalysisTest {
+
+	public static void main(String[] args) {
+		UserStatisticFactory factory = new UserStatisticFactory();
+		RelationAnalysisStatic relation = factory.GetRelationAnalysisStatic();
+		UserHandle handle = new UserHandle();
+		List<UserInfo> userInfos = new ArrayList<UserInfo>();
+		try {
+			userInfos = handle.GetAllUsers();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Relationship relationship = relation.getAllRelationship(userInfos.get(0));
+		List<RelationUser> relationUsers = relationship.getRelationUser();
+		for(RelationUser temp:relationUsers){
+			System.out.println(temp.getUserName());
+			System.out.println(temp.getPower());
+			System.out.println(temp.getLongtitude());
+		}
+	}
+}
