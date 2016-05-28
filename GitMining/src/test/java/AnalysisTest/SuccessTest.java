0f361bf3c0c26620cdd01eef9e616803448d0ffe
rww14 <rww14@software.nju.edu.cn>
2016-05-28 11:33:25 +0800

    debug:逻辑层

--- GitMining/src/test/java/AnalysisTest/SuccessTest.java
+++ GitMining/src/test/java/AnalysisTest/SuccessTest.java
@@ -24,7 +24,7 @@ public class SuccessTest {
 		List<LanguageStatistics> languageStatistics = SuccStatic.getLanguageStat();
 		List<CompanyStatistics> companyStatistics = SuccStatic.getCompanyStat();
 		assertEquals(250.0, one,0.0);
-		System.out.println(one);
+//		System.out.println(one);
 		//assertEquals(expected, actual);
 		for(int i=0;i < two.length;i++){
 			System.out.println(two[i]);
@@ -46,26 +46,33 @@ public class SuccessTest {
 //		System.out.println("123");
 		RepositoryStatisticFactory factory = new RepositoryStatisticFactory();
 		SuccAnalysisStatic SuccStatic = factory.GetSuccStatistcs();
+		SuccAnalysisStatic UnSuccStatic = factory.GetUnSuccStatistcs();
 		
 		
 		double one = SuccStatic.getProjectNum();
 		int[] two = SuccStatic.getCollaNum();
 		List<LanguageStatistics> languageStatistics = SuccStatic.getLanguageStat();
 		List<CompanyStatistics> companyStatistics = SuccStatic.getCompanyStat();
-		System.out.println(one);
+		List<CompanyStatistics> companyStatistics2 = UnSuccStatic.getCompanyStat();
+//		System.out.println(one);
 		for(int i=0;i < two.length;i++){
-			System.out.println(two[i]);
+//			System.out.println(two[i]);
 		}
 		//System.out.println(two[two.length-1]);
 		//System.out.println(companyStatistics.size());
 		for(int i=0;i < (languageStatistics.size());i++){
 //			System.out.println(i);
-			System.out.println(languageStatistics.get(i).getLanguage());
-			System.out.println(languageStatistics.get(i).getNum());
+//			System.out.println(languageStatistics.get(i).getLanguage());
+//			System.out.println(languageStatistics.get(i).getNum());
 		}
 		for(int i=0;i<companyStatistics.size();i++){
 			System.out.println(companyStatistics.get(i).getCompany());
 			System.out.println(companyStatistics.get(i).getNum());
+		}
+		System.out.println("123");
+		for(int i=0;i<companyStatistics2.size();i++){
+			System.out.println(companyStatistics2.get(i).getCompany());
+			System.out.println(companyStatistics2.get(i).getNum());
 		}
 	}
 
